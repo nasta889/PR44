d7279b8c4a728e60d1e81e6fcc859305d5a462a4
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 15:36:33 +0200

    Update Product.java

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -1,2 +1,19 @@
 package PACKAGE_NAME;public class Product {
 }
+public abstract class Product {
+    private String name;
+    private double price;
+
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
