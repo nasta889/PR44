5fe71e2b9ddde0eb9150ab09bbc3a365ac1eff7b
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 15:34:13 +0200

    Update Clothing.java

--- src/main/java/Clothing.java
+++ src/main/java/Clothing.java
@@ -1,2 +1,17 @@
 package PACKAGE_NAME;public class Clothing {
 }
+import lombok.Builder;
+
+public class Clothing extends Product {
+    private String size;
+
+    @Builder
+    public Clothing(String name, double price, String size) {
+        super(name, price);
+        this.size = size;
+    }
+
+    public String getSize() {
+        return size;
+    }
+}
