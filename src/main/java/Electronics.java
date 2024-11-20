a4d431798f9cad80fc616e933eb261e5638027f2
nasta889 <161520096+nasta889@users.noreply.github.com>
2024-11-20 15:35:32 +0200

    Update Electronics.java

--- src/main/java/Electronics.java
+++ src/main/java/Electronics.java
@@ -1,2 +1,17 @@
 package PACKAGE_NAME;public class Electronics {
 }
+import lombok.Builder;
+
+public class Electronics extends Product {
+    private String brand;
+
+    @Builder
+    public Electronics(String name, double price, String brand) {
+        super(name, price);
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+}
